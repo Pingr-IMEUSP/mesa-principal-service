b1e719eb4d5c99ad28512b17fcd1345f7f6ab093
Thiago Guerrero <54722457+T-Guerrero@users.noreply.github.com>
2021-02-22 17:17:03 -0300

    Update controller.ts

--- src/controller.ts
+++ src/controller.ts
@@ -1,7 +1,7 @@
 import { Context } from 'koa';
 import { Ping } from './db/mockdb';
 
-export default class PingClass {
+export default class PingController {
   static index(ctx: Context): void {
     ctx.body = Ping.all;
   }
