c79fde9eec55fcf7b5a3919b6e84ee70fea96e53
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-02-22 19:35:48 -0300

    add KEYWORD_CREATED event listener
    Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
    Co-authored-by: Thiago Guerrero <tgbalera@hotmail.com>
    Co-authored-by: Abigail Lopes <contatoabigail_lopes@outlook.com>
    Co-authored-by: Jefferson Lopes <jeff.lopes@outlook.com>
    Co-authored-by: Bruno Romano <brunopromano@gmail.com>
    Co-authored-by: dnbtr <daniel.bittar@alumni.usp.br>

--- src/db/mockdb.ts
+++ src/db/mockdb.ts
@@ -22,3 +22,14 @@ export class User {
   static count = 0;
   static all: UserInterface[] = [];
 }
+
+export interface KeywordInterface {
+  id: number;
+  keyword: string;
+  timestamp: Date;
+}
+
+export class Keyword {
+  public static count = 0;
+  public static all: KeywordInterface[] = [];
+}
--- src/nats-streaming/listeners.ts
+++ src/nats-streaming/listeners.ts
@@ -1,5 +1,5 @@
 import { Message } from 'node-nats-streaming';
-import { Ping, User, PingInterface, UserInterface } from '../db/mockdb';
+import { Ping, User, Keyword, PingInterface, UserInterface, KeywordInterface } from '../db/mockdb';
 
 import { stan } from './stan';
 
@@ -8,6 +8,7 @@ export function setupListeners(): void {
 
   const createdPing = stan.subscribe('PING_CREATED', replayAllOpts);
   const createdUser = stan.subscribe('USER_CREATED', replayAllOpts);
+  const createdKeyword = stan.subscribe('KEYWORD_CREATED', replayAllOpts);
 
   createdPing.on(
     'message',
@@ -27,6 +28,16 @@ export function setupListeners(): void {
 
       await User.all.push(user);
       console.log('[USER_CREATED]:', user);
+    },
+  );
+
+  createdKeyword.on(
+    'message',
+    async (msg: Message): Promise<void> => {
+      const keyword: KeywordInterface = JSON.parse(msg.getData() as string);
+
+      await Keyword.all.push(keyword);
+      console.log('[KEYWORD_CREATED]:', keyword);
     },
   );
 }
